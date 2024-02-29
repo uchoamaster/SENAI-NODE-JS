ee2a2059342f988182790bba58bbdd42a516e036
unknown <gustavolscmidt13@gmail.com>
2024-02-28 21:16:15 -0300

    28/02/24_21:16

--- Gustavo Luis/Lista_de_games/server.js
+++ Gustavo Luis/Lista_de_games/server.js
@@ -19,16 +19,16 @@ let games = [
 ]
 
 let myGames = [
-    { "title": "The Witcher 3: Wild Hunt", "developer": "CD Projekt Red", "price": 29.99 },
-    { "title": "Counter-Strike: Global Offensive", "developer": "Valve", "price": 0.00 },
-    { "title": "Among Us", "developer": "Innersloth", "price": 4.99 },
-    { "title": "Grand Theft Auto V", "developer": "Rockstar North", "price": 29.99 },
-    { "title": "Red Dead Redemption 2", "developer": "Rockstar Games", "price": 59.99 },
-    { "title": "PlayerUnknown's Battlegrounds", "developer": "PUBG Corporation", "price": 29.99 },
-    { "title": "Fall Guys: Ultimate Knockout", "developer": "Mediatonic", "price": 19.99 },
-    { "title": "Sid Meier's Civilization VI", "developer": "Firaxis Games", "price": 59.99 },
-    { "title": "Terraria", "developer": "Re-Logic", "price": 9.99 },
-    { "title": "Dota 2", "developer": "Valve", "price": 0.00 }
+    { title: "The Witcher 3: Wild Hunt", Desenvolvedora: "CD Projekt Red", price: 29.99 },
+    { title: "Counter-Strike: Global Offensive", Desenvolvedora: "Valve", price: 0.00 },
+    { title: "Among Us", Desenvolvedora: "Innersloth", price: 4.99 },
+    { title: "Grand Theft Auto V", Desenvolvedora: "Rockstar North", price: 29.99 },
+    { title: "Red Dead Redemption 2", Desenvolvedora: "Rockstar Games", price: 59.99 },
+    { title: "PlayerUnknown's Battlegrounds", Desenvolvedora: "PUBG Corporation", price: 29.99 },
+    { title: "Fall Guys: Ultimate Knockout", Desenvolvedora: "Mediatonic", price: 19.99 },
+    { title: "Sid Meier's Civilization VI", Desenvolvedora: "Firaxis Games", price: 59.99 },
+    { title: "Terraria", Desenvolvedora: "Re-Logic", price: 9.99 },
+    { title: "Dota 2", Desenvolvedora: "Valve", price: 0.00 }
 ]
 
 app.get("/", (req, res) => {
